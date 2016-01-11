b3d1d1c862a35be51505ae1919e6a35b8b3e8ee0
Neil Larion and Oskar Radon <neil.larion@gmail.com>
2016-01-11 15:53:01 -0800

    Fix glyphicon rotation animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,29 @@
 // function that shows definition
+var checkState = true;
 $(function() {
   $(".title").click(function() {
     $(this).next().slideToggle();
-    $(this).prev().animate({  borderSpacing: -90 }, {
-      step: function(now,fx) {
-        $(this).css('-webkit-transform','rotate('+now+'deg)');
-        $(this).css('-moz-transform','rotate('+now+'deg)');
-        $(this).css('transform','rotate('+now+'deg)');
-      },
-      duration:'slow'
-    });
+    if (checkState) {
+      $(this).prev().animate({  borderSpacing: -90 }, {
+        step: function() {
+          $(this).css('-webkit-transform','rotate(90deg)');
+          $(this).css('-moz-transform','rotate(90deg)');
+          $(this).css('transform','rotate(90deg)');
+        },
+        duration:'slow'
+      });
+      checkState = false;
+    }else {
+      console.log("negaitve 90"+checkState)
+      $(this).prev().animate({  borderSpacing: -90 }, {
+        step: function(now,fx) {
+          $(this).css('-webkit-transform','rotate(0deg)');
+          $(this).css('-moz-transform','rotate(0deg)');
+          $(this).css('transform','rotate(0deg)');
+        },
+        duration:'slow'
+      });
+      checkState = true;
+    }
   });
 });
