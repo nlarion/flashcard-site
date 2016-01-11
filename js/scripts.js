d391403886cf9af8e08b96c02319112ab218d2e5
Neil Larion and Oskar Radon <oskarradonkimball@gmail.com>
2016-01-11 15:40:33 -0800

    Add triangle glyphicon and rotate animation.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 // function that shows definition
 $(function() {
   $(".title").click(function() {
-    $(this).next().toggle();
+    $(this).next().slideToggle();
+    $(this).prev().animate({  borderSpacing: -90 }, {
+      step: function(now,fx) {
+        $(this).css('-webkit-transform','rotate('+now+'deg)');
+        $(this).css('-moz-transform','rotate('+now+'deg)');
+        $(this).css('transform','rotate('+now+'deg)');
+      },
+      duration:'slow'
+    });
   });
 });
